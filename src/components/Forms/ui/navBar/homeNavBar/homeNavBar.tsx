b095a628d133bd54244079ade2d45ad63d7afa3e
DatBrian <vikard.2005@hotmail.com>
2025-07-24 21:12:29 -0500

    feat: :sparkles: Home page completed
    The design of the home page has been completed

--- src/components/Forms/ui/navBar/homeNavBar/homeNavBar.tsx
+++ src/components/Forms/ui/navBar/homeNavBar/homeNavBar.tsx
@@ -1,21 +1,27 @@
-const homeNavBar = () => {
+import "./HomeNavBar.css"
+
+const HomeNavBar = () => {
 	return (
-		<nav className="home-nav-bar">
-			<div className="nav-container">
-				<a href="/" className="nav-logo">
-					Zinobe
-				</a>
-				<ul className="nav-links">
-					<li>
-						<a href="/about">About</a>
-					</li>
-					<li>
-						<a href="/contact">Contact</a>
-					</li>
-				</ul>
+		<nav id="homeNavBar" className="home-nav-bar py-5">
+			<div className="nav-container flex">
+				<div className="w-full flex">
+
+				<div className="px-6 h-full" id="logo">
+					<img src="src\assets\images\logo2.png" width={100} alt="logo"/>
+				</div>
+				<div className="flex justify-around items-center" id="navButtons">
+					<a href="">Home</a>
+					<a href="">About</a>
+					<a href="">Help</a>
+
+				</div>
+				</div>
+				<div className="px-10" id="loginButton">
+					<button >Login</button>
+				</div>
 			</div>
 		</nav>
 	);
 };
 
-export default homeNavBar;
+export default HomeNavBar;
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,10 +1,12 @@
+
+import HomeNavBar from "../../components/Forms/ui/navBar/homeNavBar/HomeNavBar";
 import "./HomePage.css";
 
 const HomePage = () => {
 	return (
 		<>
+				<HomeNavBar/>
 			<section className="flex h-screen w-screen">
-				<div></div>
 				<div
 					id="homeImg"
 					className="hidden lg:block h-full overflow-hidden"
@@ -14,6 +16,17 @@ const HomePage = () => {
 						alt="Auth background"
 						className="w-full h-full object-cover"
 					/>
+				</div>
+				<div className="bg-white h-full" id="homeText">
+					<div className="p-10 mt-14" id="welcomeText">
+						<p className="text-8xl">Fast and secure loans </p>
+						<div className="flex text-8xl gap-5">
+							<p id="percent" className="italic w-fit">100%</p> <p className="w-fit">online</p>
+						</div>
+					</div>
+					<div className="flex justify-center" >
+						<button id="getStartedButton" className="bold italic">GET STARTED</button>
+					</div>
 				</div>
 			</section>
 			;
